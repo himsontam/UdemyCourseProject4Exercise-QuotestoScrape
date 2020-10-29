28994e5bfc031fad0539228812e2bd5879865f14
himson Tam <himsom0528@gmail.com>
2020-10-29 22:06:21 +0800

    scrapy script

--- QuotestoScrape/spiders/quotes.py
+++ QuotestoScrape/spiders/quotes.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy_splash import SplashRequest
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['http://quotes.toscrape.com/js']
-    start_urls = ['http://http://quotes.toscrape.com/js/']
+    allowed_domains = ['quotes.toscrape.com']
+    # start_urls = ['http://http://quotes.toscrape.com/js/']
+    
+    script = '''
+        function main(splash, args)
+            assert(splash:go(args.url))
+            assert(splash:wait(0.5))
+            return splash:html()
+        end
+    '''
+    def start_requests(self):
+        yield SplashRequest(url="http://quotes.toscrape.com/js/", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
 
     def parse(self, response):
-        pass
+        # quotes = response.xpath("//div[@class='quote']")
+        for quote in response.xpath("//div[@class='quote']"):
+            yield {
+                'text': quote.xpath(".//span[@class='text']/text()").get(),
+                'author': quote.xpath(".//span/small[@class='author']/text()").get(),
+                'tag': quote.xpath(".//div[@class='tags']/a/text()").getall()
+            }
+        next_page = response.xpath("//li[@class='next']/a/@href").get()
+        if next_page:
+            absolute_url = f"http://quotes.toscrape.com{next_page}"
+            yield SplashRequest(url=absolute_url, callback=self.parse, endpoint="execute", args={
+                'lua_source': self.script
+            })
